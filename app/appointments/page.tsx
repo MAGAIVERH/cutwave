a9a1a4f7715a9596296968b8a949aca24acc4b0d
Magaiver <magaivermagalhaes.mm@gmail.com>
2025-12-09 23:03:17 -0300

    refactor: separate booking data fetching from BookingItem presentation

--- app/appointments/page.tsx
+++ app/appointments/page.tsx
@@ -42,7 +42,7 @@ export default async function AppointmentsPage() {
       },
     },
     orderBy: {
-      date: "desc",
+      date: "asc",
     },
   });
 
--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ const Home = async () => {
           },
         },
         orderBy: {
-          date: "desc",
+          date: "asc",
         },
         take: 5,
       })
